3765e1e69ec5045dbbc6337c49cf134e1bf2c092
Aliaksandr Senkevich <puskin1914@gmail.com>
2021-04-20 20:15:23 +0300

    refactor Auth as a func component

--- src/components/Auth/Auth.jsx
+++ src/components/Auth/Auth.jsx
@@ -1,46 +1,46 @@
 import React from 'react';
 
-export default class Auth extends React.Component {
-  onEmailChange = (event) => {
-    this.props.setEmail(event.target.value);
+const Auth = ({ email, password, setEmail, setPassword }) => {
+  const onEmailChange = (event) => {
+    setEmail(event.target.value);
   };
 
-  onPasswordChange = (event) => {
-    this.props.setPassword(event.target.value);
+  const onPasswordChange = (event) => {
+    setPassword(event.target.value);
   };
 
-  render() {
-    return (
-      <div className="auth">
-        <h3>Sign In</h3>
-        <form>
-          <div>
-            <input
-              type="text"
-              name="login"
-              value={this.props.email}
-              autoComplete="off"
-              placeholder="E-mail"
-              spellCheck={false}
-              onChange={this.onEmailChange}
-            />
-          </div>
+  return (
+    <div className="auth">
+      <h3>Sign In</h3>
+      <form>
+        <div>
+          <input
+            type="text"
+            name="login"
+            value={email}
+            autoComplete="off"
+            placeholder="E-mail"
+            spellCheck={false}
+            onChange={onEmailChange}
+          />
+        </div>
 
-          <div>
-            <input
-              type="password"
-              name="password"
-              value={this.props.password}
-              placeholder="Password"
-              onChange={this.onPasswordChange}
-            />
-          </div>
+        <div>
+          <input
+            type="password"
+            name="password"
+            value={password}
+            placeholder="Password"
+            onChange={onPasswordChange}
+          />
+        </div>
 
-          <div>
-            <button>Sign In</button>
-          </div>
-        </form>
-      </div>
-    );
-  }
-}
+        <div>
+          <button>Sign In</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Auth;
